16bf8b27ffde0afd76a70ee0306e5f9a702bd38e
Nikunj Saraf <Nikunj.Saraf@in.ey.com>
2023-03-14 15:17:54 +0530

    Create asset manager

---
+++ AssetManager.cpp
@@ -0,0 +1,34 @@
+#include "AssetManager.h"
+
+namespace Nikunj
+{
+	void Nikunj::AssetManager::LoadTexture(std::string textureName, std::string fileName)
+	{
+		sf::Texture texture;
+
+		if (texture.loadFromFile(fileName))
+		{
+			this->_textures[textureName] = texture;
+		}
+	}
+
+	sf::Texture& Nikunj::AssetManager::GetTexture(std::string textureName)
+	{
+		return this->_textures.at(textureName);
+	}
+
+	void Nikunj::AssetManager::LoadFont(std::string fontName, std::string fileName)
+	{
+		sf::Font font;
+
+		if (font.loadFromFile(fileName))
+		{
+			this->_fonts[fontName] = font;
+		}
+	}
+
+	sf::Font& Nikunj::AssetManager::GetFont(std::string fontName)
+	{
+		return this->_fonts.at(fontName);
+	}
+}
