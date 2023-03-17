87ce04f9219fe57758e2d9faed1e5b2985cf20a8
Nikunj Saraf <Nikunj.Saraf@in.ey.com>
2023-03-17 17:11:49 +0530

    finish project

--- Definitions.h
+++ Definitions.h
@@ -9,19 +9,19 @@ constexpr auto SCREEN_HEIGHT = 960;
 
 constexpr auto SPLASH_STATE_SHOW_TIME = 3;
 
-constexpr auto SPLASH_SCENE_BACKGROUND_FILEPATH = "./Resources/res/Splash Background.png";
+constexpr auto SPLASH_SCENE_BACKGROUND_FILEPATH = "Resources/res/Splash Background.png";
 
-constexpr auto MAIN_MENU_BACKGROUND_FILEPATH = "./Resources/res/Main Menu Background.png";
-constexpr auto MAIN_MENU_TITLE_FILEPATH = "./Resources/res/Game Title.png";
-constexpr auto MAIN_MENU_PLAY_BUTTON = "./Resources/res/Play Button.png";
-constexpr auto MAIN_MENU_PLAY_BUTTON_OUTER = "./Resources/res/Play Button Outer.png";
+constexpr auto MAIN_MENU_BACKGROUND_FILEPATH = "Resources/res/Main Menu Background.png";
+constexpr auto MAIN_MENU_TITLE_FILEPATH = "Resources/res/Game Title.png";
+constexpr auto MAIN_MENU_PLAY_BUTTON = "Resources/res/Play Button.png";
+constexpr auto MAIN_MENU_PLAY_BUTTON_OUTER = "Resources/res/Play Button Outer.png";
 
-constexpr auto GAME_BACKGROUND_FILEPATH = "./Resources/res/Main Menu Background.png";
-constexpr auto PAUSE_BUTTON = "./Resources/res/Pause Button.png";
-constexpr auto X_PIECE_FILEPATH = "./Resources/res/X.png";
-constexpr auto O_PIECE_FILEPATH = "./Resources/res/O.png";
-constexpr auto X_WINNING_PIECE_FILEPATH = "./Resources/res/X Win.png";
-constexpr auto O_WINNING_PIECE_FILEPATH = "./Resources/res/O Win.png";
+constexpr auto GAME_BACKGROUND_FILEPATH = "Resources/res/Main Menu Background.png";
+constexpr auto PAUSE_BUTTON = "Resources/res/Pause Button.png";
+constexpr auto X_PIECE_FILEPATH = "Resources/res/X.png";
+constexpr auto O_PIECE_FILEPATH = "Resources/res/O.png";
+constexpr auto X_WINNING_PIECE_FILEPATH = "Resources/res/X Win.png";
+constexpr auto O_WINNING_PIECE_FILEPATH = "Resources/res/O Win.png";
 
 const auto EMPTY_PIECE = -1;
 
@@ -36,11 +36,11 @@ constexpr auto STATE_AI_PLAYING = 93;
 constexpr auto STATE_DRAW = 92;
 
 
-constexpr auto PAUSE_BACKGROUND_FILEPATH = "./Resources/res/Main Menu Background.png";
-constexpr auto RESUME_BUTTON = "./Resources/res/Resume Button.png";
-constexpr auto HOME_BUTTON = "./Resources/res/Home Button.png";
-constexpr auto RETRY_BUTTON = "./Resources/res/Retry Button.png";
+constexpr auto PAUSE_BACKGROUND_FILEPATH = "Resources/res/Main Menu Background.png";
+constexpr auto RESUME_BUTTON = "Resources/res/Resume Button.png";
+constexpr auto HOME_BUTTON = "Resources/res/Home Button.png";
+constexpr auto RETRY_BUTTON = "Resources/res/Retry Button.png";
 
-constexpr auto GRID_SPRITE = "./Resources/res/Grid.png";
+constexpr auto GRID_SPRITE = "Resources/res/Grid.png";
 
 constexpr auto TIME_BEFORE_SHOWING_GAME_OVER = 3;
